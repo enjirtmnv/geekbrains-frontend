395675bbd3b862534c6066c6c7758300dec1a303
enjirtmnv <akijda@mail.ru>
2018-06-13 21:31:39 +0300

    task1 output changed

--- task1.js
+++ task1.js
@@ -2,5 +2,9 @@ var inputStr = process.argv[2].toLowerCase() // приводим к нижнем
 inputStr = inputStr.replace(/[^a-zа-яё]/g, ''); // оставляем только буквы для проверки палиндрома
 var reverseStr = inputStr.split('').reverse().join('') // переворачиваем
 var ans = (reverseStr === inputStr) // проверяем на палиндром
-process.stdout.write(ans.toString()) // вывод ответа
-process.stdout.write("\n")
+if (ans) { // вывод ответа
+  process.stdout.write("YES")
+} else {
+  process.stdout.write("NO") 
+}
+  
