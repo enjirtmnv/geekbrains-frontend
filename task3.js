6e8539f2312636adb8fa42e432e2a25fdb0c9a7f
enjirtmnv <akijda@mail.ru>
2018-06-16 20:28:04 +0300

    bug with 111 fixed

--- task3.js
+++ task3.js
@@ -1,12 +1,6 @@
 var a = parseInt(process.argv[2]) // приводим к нижнему регистру
 var b = parseInt(process.argv[3]) // приводим к нижнему регистру
 var c = a+b
-if (c == 0) // обработаем крайний случай
-{
-	// process.stdout.write("÷0 секунд")
-	return;
-}
-
 
 var hours = ~~(c / 3600) // количество часов
 var minutes = ~~(c / 60) - 60*hours // количество минут
@@ -14,50 +8,38 @@ var seconds = c%60 // количество секунд
 
 var output = "" //+ " " + minutes.toString() + " " + seconds.toString()
 
-// можно было бы сделать функцию, но часы и минуты имеют разные окончания, поэтому решила сделать копипастой
-
-// час -- 1, 21, 31, ... оканчивается на 1 (кроме 11)
-// часа -- оканчивается на 2, 3, 4, кроме 12 13 14 
-// часов все остальное
 if (hours > 0)
 {
 	output += hours.toString()
-	if ((hours != 11) && ((hours % 10) == 1) ){
+	if ((hours%100 != 11) && ((hours % 10) == 1) ){
 	  output += " час ";
-	} else if (([2, 3, 4].includes(hours % 10)) && (!([12, 13, 14].includes(hours)))){
+	} else if (([2, 3, 4].includes(hours % 10)) && (!([12, 13, 14].includes(hours % 100)))){
 	  output += " часа ";
 	} else {
 	  output += " часов ";
 	}
 }
-// минуты -- оканчивается на 2,3,4 кроме 12 13 14
-// минута -- оканчивается на 1, кроме 11
-// минут -- все остальное
 if  ((minutes > 0))
 {
 	output += minutes.toString()
-	if ((minutes != 11) && ((minutes % 10) == 1) ){
+	if ((minutes%100 != 11) && ((minutes % 10) == 1) ){
 	  output += " минута ";
-	} else if (([2, 3, 4].includes(minutes % 10)) && (!([12, 13, 14].includes(minutes)))){
+	} else if (([2, 3, 4].includes(minutes % 10)) && (!([12, 13, 14].includes(minutes% 100)))){
 	  output += " минуты ";
 	} else {
 	  output += " минут ";
 	}
 }
-// секунда -- оканчивается на 1 кроме 11 
-// секунды -- оканчивается на 2 3 4 кроме 12 13 14
-// секунд -- все остальное
-// if (seconds > 0)
 if (seconds > 0)
 {
 	output += seconds.toString()
-	if ((seconds != 11) && ((seconds % 10) == 1) ){
+	if ((seconds%100 != 11) && ((seconds % 10) == 1) ){
 	  output += " секунда";
-	} else if (([2, 3, 4].includes(seconds % 10)) && (!([12, 13, 14].includes(seconds)))){
+	} else if (([2, 3, 4].includes(seconds % 10)) && (!([12, 13, 14].includes(seconds% 100)))){
 	  output += " секунды";
 	} else {
 	  output += " секунд";
 	}
 }
 
-process.stdout.write(output)
+process.stdout.write(output.trim())
